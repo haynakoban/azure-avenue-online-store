b10fae932ee4775ca82a877bf43a56c686e86c5c
Bryan Cortez <bancortez2@gmail.com>
2023-05-09 09:11:10 +0800

    fixed dropdown

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Azure Avenue</title>
-    <script src="//unpkg.com/alpinejs" defer></script>
+    <script src="//unpkg.com/alpinejs"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="{{ asset('js/main.js') }}"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
