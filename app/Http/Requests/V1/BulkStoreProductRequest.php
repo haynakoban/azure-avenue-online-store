1a9f5233b019d2dd5c8d01f209b0a4a6651ec415
Bryan Cortez <bancortez2@gmail.com>
2023-05-15 14:33:23 +0800

    authorization

--- app/Http/Requests/V1/BulkStoreProductRequest.php
+++ app/Http/Requests/V1/BulkStoreProductRequest.php
@@ -11,7 +11,9 @@ class BulkStoreProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        $user = $this->user();
+
+        return $user != null && $user->tokenCan('create');
     }
 
     /**
